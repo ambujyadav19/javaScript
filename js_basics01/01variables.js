2935e54c7de2616a0c5aafdc1cd94992342115db
ambujyadav19 <yadavambuj503@gmail.com>
2025-05-26 10:23:39 +0000

    02_datatypes

---
+++ js_basics01/01variables.js
@@ -0,0 +1,21 @@
+const accountId=132443
+let accountEmail="ambuj@google.com"
+var accountPassword="Aryan123"
+accountCity="Ballia"
+
+
+// accountId=2 //not allowed
+accountEmail="aryan@google.com"
+accountPassword="465452"
+accountCity="mau"
+let accountState
+
+console.log(accountId)
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ js_basics01/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";/// treat all JS CODE AS NEWER VERSION
+
+// alert(3+3)// we are using nodejs,not browser
+
+console.log(3+3);
+console.log("aryan")
+
+///code readability should be high
+let name="aryan"
+let age=20
+let isloggedIn=true
+
+//number =>2 to power 53
+//bigint
+///string=>""
+//boolean=>true/false
+//null=>is a stsndalone value,null is a object itself.
+
+let a=null
+//undefined=>
+//symbol=>unique
+
+
+//object
+
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
