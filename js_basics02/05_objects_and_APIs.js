359c2489b549a2e23d4b7972a9db93da057face9
ambujyadav19 <yadavambuj503@gmail.com>
2026-01-02 13:57:04 +0000

    JSON talk

---
+++ js_basics02/05_objects_and_APIs.js
@@ -0,0 +1,21 @@
+const course={
+    coursename:"js in hindi",
+    courseFee:"999",
+    courseInstructor:"Hitesh"
+}
+// console.log(course.courseInstructor)
+
+//De-structuring
+const{courseInstructor}=course
+console.log(courseInstructor);
+
+
+//+++++++++++APIs++++++++++++
+
+
+// {
+//     name:"ambuj",
+//     coursename:"js in hindi",
+//     price:"free"
+// }
+
