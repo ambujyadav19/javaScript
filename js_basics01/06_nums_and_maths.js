2fa12d06d1bcb9cf5a1d2751f60cc8cb03c17a1b
ambujyadav19 <yadavambuj503@gmail.com>
2026-01-02 09:52:15 +0000

    num and maths

--- js_basics01/06_nums_and_maths.js
+++ js_basics01/06_nums_and_maths.js
@@ -1,6 +1,8 @@
 const score=400
 
 const balance=new Number(100)
+// here 'balance' is NUmber object here.
+
 // console.log(balance);
 // console.log(balance.toString().length);
 
@@ -20,6 +22,7 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.65))
 console.log(Math.ceil(4.4))
 console.log(Math.floor(4.4))
+console.log(Math.min(2,4,3,-1,0))
 
 console.log((Math.random()*10)+1);
 
