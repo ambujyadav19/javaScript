4e38f343f4dc2abea70f2b266db15f51cd8b2f6a
ambujyadav19 <yadavambuj503@gmail.com>
2026-01-02 08:31:12 +0000

    stack and heap

--- js_basics01/datatypes-summery.js
+++ js_basics01/datatypes-summery.js
@@ -1,5 +1,5 @@
-//primitive data types=>
-    //7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+//1.primitive data types=>(these all are call by value)
+    //7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
 
 const score=100
 const scoreValue=100.3
@@ -11,28 +11,37 @@ const Id=Symbol('123')
 const anotherId=Symbol('123')
 
 console.log(Id===anotherId);
+//here Id and anotherId are not same because the return type of both is different because of 'Symbol'
 
-// const bigNumber=384653195136063876n
-console.log(typeof bigNumber)
+const bigNumber=384653195136063876n
+// console.log(typeof bigNumber)
 
-//reference type or non premitive=>
+//2.Reference type or Non premitive=>
     //Arrays,Objects,Finctions
-
+//array 
 const heros=["shaktiman","naagraj","doga"];
 
+
+//object
 let myObj={
      name:"ambuj",
      age:22,
 }
+
+//function
 const myFunction=function(){
     console.log("hello world");
 }
 
-console.log(typeof myObj)
+console.log(typeof heros)
 
 
 //++++++++++++++++++++
-//Stack(primitive)=>copy of original memory , Heap(non primitive)=> reference of originam memory
+//two type of memory in JS 
+//1.stack memory =>copy hi milta hai
+//2.Heap memory=>isme change original value me hi hota hai
+//Stack(primitive data types)=>copy of original memory , 
+//  Heap(non primitive)=> reference of originam memory
 
 let vill_name="rampur"
 let another ="barauli"
@@ -43,7 +52,7 @@ console.log(another)
 console.log(vill_name)
 
 let userOne={
-    email:"yser@google.com",
+    email:"user@google.com",
     upi:"67574324@abl"
 }
  usertwo=userOne
